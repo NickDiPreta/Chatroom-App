fbec05178bad45e83c511d582c55af5ec8ffc670
nick <nicholasdipreta2020@gmail.com>
2020-06-27 18:40:41 -0400

    Updates

---
+++ precache-manifest.344217f52f704bf548457432d864f480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8314cbf1b6035b52d0bb5be5de1a6bc",
+    "url": "/Chatroom-App/index.html"
+  },
+  {
+    "revision": "9bfa29f8d7e876c0a463",
+    "url": "/Chatroom-App/static/css/main.da67cd58.chunk.css"
+  },
+  {
+    "revision": "7d24eaaea0b3bb5e5988",
+    "url": "/Chatroom-App/static/js/2.983c804f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chatroom-App/static/js/2.983c804f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bfa29f8d7e876c0a463",
+    "url": "/Chatroom-App/static/js/main.c4503682.chunk.js"
+  },
+  {
+    "revision": "8e4b3d75e8ac93207cb7",
+    "url": "/Chatroom-App/static/js/runtime-main.a6f136b8.js"
+  }
+]);
