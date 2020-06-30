8fae539604e3df927936d961dc36ea7687e8d415
nick <nicholasdipreta2020@gmail.com>
2020-06-30 13:59:08 -0400

    bolded historical message usernames removed name from sender UI text

--- src/Messages.js
+++ src/Messages.js
@@ -11,13 +11,14 @@ const Messages = (props) => {
     console.log(message);
     let time = ConvertTimeStamp(message[1]);
     if (message[0] != null) {
+      let halfs = message[0].split(":")
       return (
         <li className="Messages-message">
           <span className="avatar" style={{ backgroundColor: randomColor() }} />
           
           <div className="Message-content">
-          <div className="timestamp">{time}</div>
-            <div className="text">{message[0]}</div>
+          <div className="username">{time}</div>
+            <div className="text"><span className="bold">{halfs[0]}</span>{":" + halfs[1]}</div>
           </div>
         </li>
       );
@@ -29,12 +30,13 @@ const Messages = (props) => {
         ? "Messages-message currentMember"
         : "Messages-message";
       const username = member.clientData.username;
+      let newText = text.replace(`${username} :`,"")
       return (
         <li className={className}>
           <span className="avatar" style={{ backgroundColor: member.color }} />
           <div className="Message-content">
             <div className="username">{username}</div>
-            <div className="text">{text}</div>
+            <div className="text">{newText}</div>
           </div>
         </li>
       );
