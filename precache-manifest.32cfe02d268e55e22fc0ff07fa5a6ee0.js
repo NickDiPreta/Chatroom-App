1db96e65d510af8c6b0aae340e563070674fa117
nick <nicholasdipreta2020@gmail.com>
2020-07-01 20:49:52 -0400

    Updates

--- precache-manifest.32cfe02d268e55e22fc0ff07fa5a6ee0.js
+++ precache-manifest.32cfe02d268e55e22fc0ff07fa5a6ee0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb9fc2bae6526f5488140124e21116ba",
+    "revision": "d457f7de1e040d84061e1a10a9115052",
     "url": "/Chatroom-App/index.html"
   },
   {
-    "revision": "6c7598d8130e94c5f5ed",
-    "url": "/Chatroom-App/static/css/main.baf0c01f.chunk.css"
+    "revision": "209c88c5c4f44553e6f3",
+    "url": "/Chatroom-App/static/css/main.bf18b48e.chunk.css"
   },
   {
-    "revision": "26b033a13868ffb297de",
-    "url": "/Chatroom-App/static/js/2.c791e702.chunk.js"
+    "revision": "8cf2f954cd0e1123fec0",
+    "url": "/Chatroom-App/static/js/2.f085e241.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/Chatroom-App/static/js/2.c791e702.chunk.js.LICENSE.txt"
+    "url": "/Chatroom-App/static/js/2.f085e241.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6c7598d8130e94c5f5ed",
-    "url": "/Chatroom-App/static/js/main.dff29b5d.chunk.js"
+    "revision": "209c88c5c4f44553e6f3",
+    "url": "/Chatroom-App/static/js/main.7c920a9a.chunk.js"
   },
   {
     "revision": "8e4b3d75e8ac93207cb7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Chatroom-App/precache-manifest.be778ae0d466d2c978998709f942b43c.js"
+  "/Chatroom-App/precache-manifest.32cfe02d268e55e22fc0ff07fa5a6ee0.js"
 );
 
 self.addEventListener('message', (event) => {
